9f35ccdbf638340e115bce074bedd49a601e0571
Николай Агеев <izagalskiy@mail.ru>
2022-12-27 11:35:51 +0700

    Added translite.py

---
+++ translite.py
@@ -0,0 +1,7 @@
+!pip install transformers sentencepiece
+
+from transformers import pipeline
+
+translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
+
+translator(" Я люблю узнавать новое!")
